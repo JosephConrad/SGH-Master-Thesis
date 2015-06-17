7f28e909e513a850415856cadfb27e9743e66d13
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2015-06-17 21:11:56 +0200

    UW: listings work

--- UW-Master-Thesis/praca/run.sh
+++ UW-Master-Thesis/praca/run.sh
@@ -1,4 +1,4 @@
-ssh -t ubuntu_parallels 'cd  /media/psf/Dropbox/MastersTheses/UW-Master-Thesis/praca && pdflatex -synctex=1 -interaction=nonstopmode uw.tex'
+ssh -t ubuntu_parallels 'cd  /media/psf/Dropbox/MastersTheses/UW-Master-Thesis/praca && xelatex -shell-escape -synctex=1 -interaction=nonstopmode uw.tex'
 ssh -t ubuntu_parallels 'cd  /media/psf/Dropbox/MastersTheses/UW-Master-Thesis/praca && bibtex uw.aux'
-ssh -t ubuntu_parallels 'cd  /media/psf/Dropbox/MastersTheses/UW-Master-Thesis/praca && pdflatex -synctex=1 -interaction=nonstopmode uw.tex'
+ssh -t ubuntu_parallels 'cd  /media/psf/Dropbox/MastersTheses/UW-Master-Thesis/praca && xelatex -shell-escape -synctex=1 -interaction=nonstopmode uw.tex'
 open uw.pdf
