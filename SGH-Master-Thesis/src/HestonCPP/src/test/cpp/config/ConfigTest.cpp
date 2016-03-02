02483269fec0b53f6a01b9d5e08ca199a6ab555a
Konrad Lisiecki <knrd.lisiecki@gmail.com>
2016-03-02 00:54:42 +0000

    SGH: ok

--- SGH-Master-Thesis/src/HestonCPP/src/test/cpp/config/ConfigTest.cpp
+++ SGH-Master-Thesis/src/HestonCPP/src/test/cpp/config/ConfigTest.cpp
@@ -3,17 +3,19 @@
 //
 #include <algorithm>
 #include <gtest/gtest.h>
+#include <src/main/cpp/tools/config/Config.h>
+
+TEST(cpp_sorter_test, int_arr_sort) {
+    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int eq[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int sz = sizeof(arr) / sizeof(arr[0]);
 
 
-TEST(cpp_sorter_test, int_arr_sort)
-{
-    int arr[] = {0,1,2,3,4,5,6,7,8,9};
-    int eq[]  = {0,1,2,3,4,5,6,7,8,9};
-    int sz = sizeof(arr)/sizeof(arr[0]);
-
-
-    for(int i=0; i<sz; i++){
+    for (int i = 0; i < sz; i++) {
         EXPECT_EQ(arr[i], eq[i]);
-
     }
+}
+
+TEST(tools_test, config_test) {
+    EXPECT_EQ(Config::getInstance().getSettings("Log.logfile"), "logs/log.txt");
 }
